23f7625ea067f34c22ac2ef8c430d5bc218dba3b
Sergey Minenkov <minenkov.sergey42@gmail.com>
2020-12-17 18:10:57 +0300

    added new features

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api')
-const config = require ('config')
+const config = require('config')
 const { default: Axios } = require('axios')
 
 const BOT_TOKEN = config.get('token')
@@ -7,53 +7,84 @@ const bot = new TelegramBot(BOT_TOKEN, { polling: true })
 const chat_id = config.get('chatId')
 const URL = config.get('url')
 
-let data = {}
-let botSendedStatus = false
+//Переменные хранящие статус "было ли отправлено сообщение о статусе канала в чат"
+let botSendedStatusKino = false
+let botSendedStatusMain = false
 
+//Проверка активности каналов через заданный интервал
 setInterval(() => {
-    (Axios.get(URL)
-        .then((response => {
-            if (response.data.nms.live) {
-                data = response.data.nms.live
-                if (!botSendedStatus) {
-                    sendMessage(data)
-                }
-                console.log('Stream is online')
-            } else {
-                if (botSendedStatus) {
-                    bot.sendMessage(chat_id, `Stream ended`)
-                    botSendedStatus = false
+    (Axios.get(`${URL}/channels/`)
+        .then((res => {
+            if (res.data.live[0].apps.length > 0) { //если хотябы 1 канал онлайн
+                data = res.data.live[0].apps.find(stream => stream.appName === 'live').channels
+                dataMain = data.find(channel => channel.channelName === 'main')
+                dataKino = data.find(channel => channel.channelName === 'kino')
 
-                    console.log('Stream ended')
+                if (dataMain && !botSendedStatusMain) {
+                    sendStatus(dataMain)
+                    console.log('Main is online')
+                } else if (!dataMain) {
+                    botSendedStatusMain = false
                 }
-                console.log('Stream is offline')
+                if (dataKino && !botSendedStatusKino) {
+                    sendStatus(dataKino)
+                    console.log('Kino is online')
+                } else if (!dataKino) {
+                    botSendedStatusKino = false
+                }
+            } else { //если все каналы оффлайн
+                botSendedStatusMain = false
+                botSendedStatusKino = false
+                console.log('Channels offline')
             }
         }))
-        .catch((response) => { console.log(`error: ${response}`) })
+        .catch((error) => { console.log(error) })
     )
 }, 10000)
 
-const sendMessage = (data) => {
-    if (data.kino && !botSendedStatus) {
-        bot.sendMessage(chat_id, `Stream KINO is Online \nStreamer: ${data.kino.publisher.userId}`)
-        botSendedStatus = true
+//Функция отправки статуса поднявшегося канала.
+const sendStatus = async (data) => {
+    let streamerName = await getStreamerName(data.publisher.userId)
+    sendMessage(data, streamerName)
+    console.log(`On channel: ${data.channelName}, streamer: ${streamerName}`)
+}
 
-        console.log(`Stream KINO is Online`)
-    } else if (data.main && !botSendedStatus) {
-        bot.sendMessage(chat_id, `Stream MAIN is Online \nStreamer: ${data.main.publisher.userId}`)
-        botSendedStatus = true
+//Функция получения имени стримера
+const getStreamerName = async (userId) => {
+    let name = ''
+    await Axios.get(`${URL}/users/${userId}`)
+        .then((res => {
+            name = res.data.user.name
+        }))
+    return name
+}
 
+//Функция отправки статуса в чат в зависимости от активного канала.
+const sendMessage = (data, streamerName) => {
+    if (data.channelName === 'main') {
+        bot.sendPhoto(chat_id, './assets/MainOnline.png', { caption: `Стримит: ${streamerName}` });
+        botSendedStatusMain = true
         console.log(`Stream MAIN is Online`)
+    }
+    if (data.channelName === 'kino') {
+        bot.sendPhoto(chat_id, './assets/KinoOnline.png', { caption: `Показывает: ${streamerName}` });
+        botSendedStatusKino = true
+        console.log(`Stream KINO is Online`)
     }
 }
 
+//Реакция бота на ключевые слова в чате
 bot.on('text', (msg) => {
     let userText = msg.text
-    if(userText.includes(('кабан'||'кабанчик'||'кабаняра'||'@Kabanch_bot')) && userText.includes(('стрим'||'стримы'||'стримам')) ) {
-        if (botSendedStatus) {
-            bot.sendMessage(chat_id, `Stream is Online, check KLPQ Client`)
-        } else {
-            bot.sendMessage(chat_id, `Stream is Offline`)
+    if (userText.includes(('кабан')) && userText.includes(('стрим'))) {
+        if (botSendedStatusMain) {
+            bot.sendMessage(chat_id, `На мэйне есть стрим`)
+        }
+        if (botSendedStatusKino) {
+            bot.sendMessage(chat_id, `Есть кинчик`)
+        }
+        if (!botSendedStatusMain && !botSendedStatusKino) {
+            bot.sendMessage(chat_id, `Нету стримов`)
         }
     }
 })
